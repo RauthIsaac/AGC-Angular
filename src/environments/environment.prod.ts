3d569a75d5c270aa57b84918fb0d10b1ee43c08b
Mahmoud22620 <114251031+Mahmoud22620@users.noreply.github.com>
2025-10-24 21:18:21 +0300

    ....

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://azbgulfcompany.runasp.net'
-  //apiUrl : 'https://localhost:7162'
   apiUrl:'https://api.azbgulf.com',
   baseUrl: 'https://api.azbgulf.com'
 };
